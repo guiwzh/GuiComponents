1dddd574eded58bb1f38eddcfc6058102b79231e
guiwzh <15886306338@163.com>
2024-07-26 21:57:06 +0800

    feat/使用fortawesome组件库，并对其添加主体颜色的props,使用scss中的字典和循环，批量创建不同类的样式

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,20 @@ import Button from './components/Button/button';
 import Menu from './components/Menu/menu';
 import MenuItem from './components/Menu/menuItem';
 import SubMenu from './components/Menu/subMenu';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee, faEnvelope } from '@fortawesome/free-solid-svg-icons'; 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import Icon from './components/Icon/icon';
+library.add(fas)
+
 
 function App() {
   return (
     <div className="App">
+      <Icon icon="arrow-down" theme='danger' size='lg'></Icon>
       <header className="App-header">
         <Button btnType='primary'>Heello</Button>
-
         <Menu defaultIndex='0' onSelect={(e,index) => console.log(e,index)} >
           <MenuItem>link1</MenuItem>
           <MenuItem>link2</MenuItem>

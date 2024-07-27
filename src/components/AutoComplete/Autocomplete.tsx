974cfa27c3d41c44d0c3cc2a169687b56f258dfc
guiwzh <15886306338@163.com>
2024-07-28 01:15:36 +0800

    feat/通过自定义hooks，利用useEfffect给联想记忆添加防抖的功能

--- src/components/AutoComplete/Autocomplete.tsx
+++ src/components/AutoComplete/Autocomplete.tsx
@@ -1,6 +1,7 @@
-import React, {FC,useState,ChangeEvent ,ReactElement} from 'react'
+import React, {FC,useState,ChangeEvent ,ReactElement, useEffect} from 'react'
 import Input,{InputProps} from '../Input/input'
 import Icon from '../Icon/icon';
+import useDebounce from '../../hooks/useDebounce';
 
 interface DataSourceObject {
     value: string;
@@ -16,9 +17,28 @@ export interface AutoCompletProps extends Omit<InputProps,'onSelect'> {
 export const AutoComplete: FC<AutoCompletProps> = (props) => {
     const {value,fetchSuggestions,onSelect,renderOption,...restprops} = props;
 
-    const [inputValue,setInputValue] = useState(value);
+    const [inputValue,setInputValue] = useState(value as string);
     const [suggestions,setSuggestions] = useState<DataSourceType[]>([]);
     const [loading, setloading] = useState(false);
+    const debouncedValue = useDebounce(inputValue,500);//使用该钩子函数，将inputValue的值进行防抖处理
+    useEffect(()=>{
+        async function fetchData(){
+            if (debouncedValue){
+                setloading(true)
+                try{
+                    const results = await fetchSuggestions(debouncedValue);
+                    setloading(false)
+                    setSuggestions(results);
+                }catch (e){
+                    alert(e)
+                    setloading(false)
+                }
+            }else{
+                setSuggestions([]);
+            }
+        }
+        fetchData();
+    },[debouncedValue])
     const handleSelect = (item:DataSourceType) => {
         setInputValue(item.value);
         setSuggestions([]);
@@ -29,7 +49,7 @@ export const AutoComplete: FC<AutoCompletProps> = (props) => {
     const generateDropdown = () => {
         return (
             <ul>
-                {suggestions.map((item,index)=>(
+                {suggestions?.map((item,index)=>(
                     <li key={index} onClick={()=>handleSelect(item)}>{renderOption ? renderOption(item) : item.value}
                     </li>
                 ))}
@@ -39,15 +59,6 @@ export const AutoComplete: FC<AutoCompletProps> = (props) => {
     const handleChange = async (e:ChangeEvent<HTMLInputElement>)=>{
         const value= e.target.value.trim();
         setInputValue(value)
-        if (value){
-            setloading(true)
-            const results = await fetchSuggestions(value);
-            setloading(false)
-            setSuggestions(results);
-        }else{
-            setSuggestions([]);
-        }
-        
     };
 
     return (
